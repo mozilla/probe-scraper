6dd38a5b29bbfe174aac24191509775bf0fff788
Georg Fritzsche <georg.fritzsche@gmail.com>
2017-03-15 23:53:35 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from distutils.core import setup
+
+setup(name='probe-scraper',
+      version='0.1',
+      description='Scrape probe data from Firefox repositories.',
+      author='Georg Fritzsche (gfritzsche)',
+      author_email='gfritzsche@mozilla.com',
+      url='https://github.com/georgf/probe-scraper/',
+      packages=['probe-scraper'],
+     )
