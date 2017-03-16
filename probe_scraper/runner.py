4875e893ed8a0423fffd37bf9642fadbc188474f
Georg Fritzsche <georg.fritzsche@gmail.com>
2017-03-16 22:32:46 +0000

    Accumulate parsed probe data in runner

--- probe_scraper/runner.py
+++ probe_scraper/runner.py
@@ -19,10 +19,26 @@ PARSERS = {
 
 def main(target_dir = tempfile.mkdtemp()):
     nodes = scraper.scrape(target_dir)
+    probes = {
+        # node_id -> {
+        #   histograms: {
+        #     name: ...,
+        #     ...
+        #   },
+        #   scalars: {
+        #     ...
+        #   },
+        # }
+    }
+    def add_probe_data(node_id, probe_type, probe_data):
+        if not node_id in probes:
+            probes[node_id] = {}
+        probes[node_id][probe_type] = probe_data
 
     for node_id,details in nodes.iteritems():
-        for ptype,paths in details['registries'].iteritems():
-            probes = PARSERS[ptype].parse(paths)
+        for probe_type,paths in details['registries'].iteritems():
+            probe_data = PARSERS[probe_type].parse(paths)
+            add_probe_data(node_id, probe_type, probe_data)
 
 if __name__ == "__main__":
     main('_tmp')
