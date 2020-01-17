ffb49117a5c21c63a02f148c6a4a0539c2bd48f5
Ben Wu <benjaminwu124@gmail.com>
2020-01-17 13:26:54 -0500

    Change probe expiry alerts to weekly for prev and next versions (#163)

--- probe_scraper/probe_expiry_alert.py
+++ probe_scraper/probe_expiry_alert.py
@@ -5,92 +5,108 @@
 from collections import defaultdict
 import argparse
 import datetime
+import logging
 
 import requests
 
 from probe_scraper import emailer
-from probe_scraper.scrapers import release_calendar
+from probe_scraper.parsers.utils import get_major_version
 
 FROM_EMAIL = "telemetry-alerts@mozilla.com"
 DEFAULT_TO_EMAIL = "dev-telemetry-alerts@mozilla.com"
-EMAIL_DAYS_BEFORE = 14
 PROBE_INFO_BASE_URL = "https://probeinfo.telemetry.mozilla.org/"
 
 EMAIL_BODY_FORMAT_STRING = """
-The following Firefox probes will expire in the next {expire_days} days or have already expired.
+The following Firefox probes have either expired or will expire in the next major Firefox nightly release: version {next_version} [1].
 
-{per_channel_probes}
+{probes}
 
 What to do about this:
 1. If one, some, or all of the metrics are no longer needed, please remove them from their definitions files (Histograms.json, Scalars.yaml, Events.yaml).
-2. If one, some, or all of the metrics are still required, please submit a Data Collection Review and patch to extend their expiry.
+2. If one, some, or all of the metrics are still required, please submit a Data Collection Review [2] and patch to extend their expiry.
 
 If you have any problems, please ask for help on the #fx-metrics Slack channel. We'll give you a hand.
 
 Your Friendly, Neighborhood Telemetry Team
 
-[1] https://wiki.mozilla.org/Firefox/Data_Collection
+[1] https://wiki.mozilla.org/Release_Management/Calendar
+[2] https://wiki.mozilla.org/Firefox/Data_Collection
 
 This is an automated message sent from probe-scraper.  See https://github.com/mozilla/probe-scraper for details.
 """  # noqa
 
-PER_CHANNEL_FORMAT_STRING = """
-These probes are expiring on the {channel} channel:
+PROBE_LIST_FORMAT_STRING = """
+The following probes {verb} in Firefox version {version}:
 {probes}
 """
 
 
-def find_expiring_probes(probes, release_dates, base_date=datetime.date.today(), expire_days=14):
+def get_latest_nightly_version():
+    versions = requests.get("https://product-details.mozilla.org/1.0/firefox_versions.json").json()
+    return get_major_version(versions["FIREFOX_NIGHTLY"])
+
+
+def find_expiring_probes(probes, target_version):
     """
-    Find probes expiring in the next {expire_days} days using output of the probe info service
+    Find probes expiring in the target version using output of the probe info service
 
     Returns dict of form:
     {
-      channel: {
         histogram_name: [notification_emails...]
-      }
     }
     """
-    expiring_histograms_by_channel = defaultdict(dict)
+    expiring_probes = defaultdict(dict)
     for probe in probes.values():
-        for channel, details in probe["history"].items():
-            details = details[0]
-            expiry_version = details["expiry_version"]
-            if expiry_version == "never":
-                continue
+        details = probe["history"].get("nightly")
+        if details is None or len(details) == 0:
+            continue
+        details = details[0]
+        expiry_version = details["expiry_version"]
+        if expiry_version == "never":
+            continue
 
-            expiry = release_dates[channel].get(expiry_version)
-            if expiry is None:
-                continue
-            if base_date <= expiry <= base_date + datetime.timedelta(days=expire_days):
-                expiring_histograms_by_channel[channel][probe["name"]] = (
+        if expiry_version == target_version:
+            expiring_probes[probe["name"]] = (
                     details.get("notification_emails", []) + [DEFAULT_TO_EMAIL])
 
-    return expiring_histograms_by_channel
+    return expiring_probes
 
 
-def send_emails_for_expiring_probes(expiring_histograms_by_channel, expire_days=14, dryrun=True):
-    histograms_by_email = defaultdict(lambda: defaultdict(list))
+def send_emails_for_expiring_probes(expired_probes, expiring_probes,
+                                    current_version, dryrun=True):
+    probes_by_email_by_state = defaultdict(lambda: defaultdict(list))
 
-    for channel, histograms in expiring_histograms_by_channel.items():
-        for name, emails in histograms.items():
-            for email in emails:
-                histograms_by_email[email][channel].append(name)
+    # Get expired probes for each email
+    for name, emails in expired_probes.items():
+        for email in emails:
+            probes_by_email_by_state[email]["expired"].append(name)
 
-    for email, histograms_by_channel in histograms_by_email.items():
-        per_channel_format_strings = [
-            PER_CHANNEL_FORMAT_STRING.format(channel=channel, probes="\n".join(histogram_names))
-            for channel, histogram_names
-            in histograms_by_channel.items()
-        ]
+    # Get expiring probes for each email
+    for name, emails in expiring_probes.items():
+        for email in emails:
+            probes_by_email_by_state[email]["expiring"].append(name)
+
+    for email in probes_by_email_by_state.keys():
+        probe_list_format_strings = []
+        if len(probes_by_email_by_state[email]["expiring"]) > 0:
+            probe_list_format_strings.append(
+                PROBE_LIST_FORMAT_STRING.format(
+                    verb='are expiring', version=int(current_version) + 1,
+                    probes="\n".join(probes_by_email_by_state[email]["expiring"])))
+        if len(probes_by_email_by_state[email]["expired"]) > 0:
+            probe_list_format_strings.append(
+                PROBE_LIST_FORMAT_STRING.format(
+                    verb='have expired', version=current_version,
+                    probes="\n".join(probes_by_email_by_state[email]["expired"])))
 
         email_body = EMAIL_BODY_FORMAT_STRING.format(
-            expire_days=expire_days,
-            per_channel_probes="\n".join(per_channel_format_strings)
-        )
+            next_version=int(current_version) + 1,
+            probes="\n".join(probe_list_format_strings))
 
         emailer.send_ses(FROM_EMAIL, "Telemetry Probe Expiry",
                          body=email_body, recipients=email, dryrun=dryrun)
+
+    logging.info(f"Sent emails to {len(probes_by_email_by_state)} recipients")
 
 
 def parse_args():
@@ -103,20 +119,26 @@ def parse_args():
     return parser.parse_args()
 
 
-def main(dryrun):
-    # Only run on Mondays
-    if datetime.date.today().weekday() != 1:
-        return
-
+def main(current_date, dryrun):
     probe_info = requests.get(PROBE_INFO_BASE_URL + "firefox/all/main/all_probes").json()
 
-    release_dates = release_calendar.get_release_dates()
+    current_version = get_latest_nightly_version()
+    next_version = str(int(current_version) + 1)
 
-    expiring_probes = find_expiring_probes(probe_info, release_dates, expire_days=EMAIL_DAYS_BEFORE)
+    expired_probes = find_expiring_probes(probe_info, current_version)
+    expiring_probes = find_expiring_probes(probe_info, next_version)
 
-    send_emails_for_expiring_probes(expiring_probes, EMAIL_DAYS_BEFORE, dryrun)
+    logging.info(f"Found {len(expired_probes)} expired probes in nightly {current_version}")
+    logging.info(f"Found {len(expiring_probes)} expiring probes in nightly {next_version}")
+
+    # Only send emails on Tuesdays, run the rest for debugging/error detection
+    if current_date.weekday() != 1:
+        logging.info("Skipping emails because it is not Tuesday")
+        return
+
+    send_emails_for_expiring_probes(expired_probes, expiring_probes, current_version, dryrun)
 
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.dry_run)
+    main(datetime.date.today(), args.dry_run)
--- tests/test_probe_expiry_alert.py
+++ tests/test_probe_expiry_alert.py
@@ -1,26 +1,26 @@
+from dataclasses import dataclass
 from collections import defaultdict
 from unittest import mock
 import datetime
 
 from probe_scraper import probe_expiry_alert
 
-RELEASE_DATES = {
-    "nightly": {
-        "76": datetime.date(2020, 3, 10)
-    },
-    "beta": {
-        "75": datetime.date(2020, 3, 10)
-    },
-    "release": {
-        "74": datetime.date(2020, 3, 10)
-    },
-}
+
+@dataclass
+class ResponseWrapper:
+    json_value: dict
+
+    def json(self):
+        return self.json_value
 
 
 def test_find_expiring_probes_no_expiring():
     probes = {
         "p1": {
             "history": {
+                "nightly": [{
+                    "expiry_version": "never"
+                }],
                 "beta": [{
                     "expiry_version": "never"
                 }],
@@ -32,13 +32,27 @@ def test_find_expiring_probes_no_expiring():
     }
     expiring_probes = probe_expiry_alert.find_expiring_probes(
         probes,
-        RELEASE_DATES,
+        "75",
     )
     expected = {}
     assert expiring_probes == expected
 
 
-def test_find_expiring_probes_expiring_same_day():
+def test_find_expiring_probes_channel_no_probes():
+    probes = {
+        "p1": {
+            "history": {}
+        }
+    }
+    expiring_probes = probe_expiry_alert.find_expiring_probes(
+        probes,
+        "75",
+    )
+    expected = {}
+    assert expiring_probes == expected
+
+
+def test_find_expiring_probes_expiring():
     probes = {
         "p1": {
             "name": "p1",
@@ -56,55 +70,32 @@ def test_find_expiring_probes_expiring_same_day():
         "p2": {
             "name": "p2",
             "history": {
-                "beta": [{
-                    "expiry_version": "73",
-                    "notification_emails": ["test@email.com"],
-                }],
-                "release": [{
-                    "expiry_version": "74"
-                }],
-            },
-        }
-    }
-    expiring_probes = probe_expiry_alert.find_expiring_probes(
-        probes,
-        RELEASE_DATES,
-        base_date=datetime.date(2020, 3, 10),
-        expire_days=14,
-    )
-    expected = {
-        "beta": {
-            "p1": ["test@email.com", probe_expiry_alert.DEFAULT_TO_EMAIL]
-        },
-        "release": {
-            "p2": [probe_expiry_alert.DEFAULT_TO_EMAIL]
-        },
-    }
-    assert expiring_probes == expected
-
-
-def test_find_expiring_probes_expiring_future():
-    probes = {
-        "p1": {
-            "name": "p1",
-            "history": {
-                "beta": [{
+                "nightly": [{
                     "expiry_version": "75",
                     "notification_emails": ["test@email.com"],
-                }]
+                }],
+                "beta": [{
+                    "expiry_version": "74",
+                    "notification_emails": ["test@email.com"],
+                }],
             },
-        }
+        },
+        "p3": {
+            "name": "p3",
+            "history": {
+                "nightly": [{
+                    "expiry_version": "75"
+                }],
+            },
+        },
     }
     expiring_probes = probe_expiry_alert.find_expiring_probes(
         probes,
-        RELEASE_DATES,
-        base_date=datetime.date(2020, 3, 1),
-        expire_days=14,
+        "75",
     )
     expected = {
-        "beta": {
-            "p1": ["test@email.com", probe_expiry_alert.DEFAULT_TO_EMAIL]
-        }
+        "p2": ["test@email.com", probe_expiry_alert.DEFAULT_TO_EMAIL],
+        "p3": [probe_expiry_alert.DEFAULT_TO_EMAIL],
     }
     assert expiring_probes == expected
 
@@ -114,7 +105,7 @@ def test_find_expiring_probes_use_latest_revision():
         "p1": {
             "name": "p1",
             "history": {
-                "beta": [
+                "nightly": [
                     {
                         "expiry_version": "75"
                     },
@@ -127,54 +118,32 @@ def test_find_expiring_probes_use_latest_revision():
     }
     expiring_probes = probe_expiry_alert.find_expiring_probes(
         probes,
-        RELEASE_DATES,
-        datetime.date(2020, 3, 10),
-        expire_days=14,
+        "75",
     )
     expected = {
-        "beta": {
-            "p1": [probe_expiry_alert.DEFAULT_TO_EMAIL]
-        }
+        "p1": [probe_expiry_alert.DEFAULT_TO_EMAIL]
     }
-    assert expiring_probes == expected
-
-
-def test_find_expiring_probes_version_not_found():
-    probes = {
-        "p1": {
-            "history": {
-                "beta": [{
-                    "expiry_version": "10000"
-                }]
-            }
-        }
-    }
-    expiring_probes = probe_expiry_alert.find_expiring_probes(
-        probes,
-        RELEASE_DATES,
-        datetime.date(2020, 3, 10),
-        expire_days=14,
-    )
-    expected = {}
     assert expiring_probes == expected
 
 
 @mock.patch("boto3.client")
 def test_send_email_dryrun_doesnt_send(mock_boto_client):
     expiring_probes = {
-        "beta": {
-            "p1": ["email"]
-        }
+        "p1": ["email"]
     }
     probe_expiry_alert.send_emails_for_expiring_probes(
+        {},
         expiring_probes,
+        "75",
         dryrun=False,
     )
     # make sure send_raw_email is the right method
     mock_boto_client().send_raw_email.assert_called_once()
 
     probe_expiry_alert.send_emails_for_expiring_probes(
+        {},
         expiring_probes,
+        "75",
         dryrun=True,
     )
     mock_boto_client().send_raw_email.assert_called_once()
@@ -182,14 +151,13 @@ def test_send_email_dryrun_doesnt_send(mock_boto_client):
 
 @mock.patch("probe_scraper.emailer.send_ses")
 def test_send_email(mock_send_email):
+    expired_probes = {
+        "expired_probe_1": ["email1", "email2"],
+        "expired_probe_2": ["email1"],
+    }
     expiring_probes = {
-        "beta": {
-            "expiring_probe_1": ["email1"],
-            "expiring_probe_2": ["email1"]
-        },
-        "release": {
-            "expiring_probe_2": ["email1", "email2"]
-        },
+        "expiring_probe_1": ["email1"],
+        "expiring_probe_2": ["email1"],
     }
 
     send_email_args = defaultdict(list)
@@ -200,7 +168,9 @@ def test_send_email(mock_send_email):
     mock_send_email.side_effect = update_call_args
 
     probe_expiry_alert.send_emails_for_expiring_probes(
+        expired_probes,
         expiring_probes,
+        "75",
         dryrun=True,
     )
 
@@ -212,12 +182,71 @@ def test_send_email(mock_send_email):
     assert len(send_email_args["email2"]) == 1
 
     email_body = send_email_args["email2"][0]
-    assert email_body.count("beta") == 0
-    assert email_body.count("release") == 1
-    assert email_body.count("expiring_probe_2") == 1
+    assert email_body.count("expiring_probe_1") == 0
+    assert email_body.count("expiring_probe_2") == 0
+    assert email_body.count("expired_probe_1") == 1
+    assert email_body.count("expired_probe_2") == 0
 
     email_body = send_email_args["email1"][0]
-    assert email_body.count("beta") == 1
-    assert email_body.count("release") == 1
     assert email_body.count("expiring_probe_1") == 1
-    assert email_body.count("expiring_probe_2") == 2
+    assert email_body.count("expiring_probe_2") == 1
+    assert email_body.count("expired_probe_1") == 1
+    assert email_body.count("expired_probe_2") == 1
+
+
+@mock.patch("requests.get")
+@mock.patch("probe_scraper.probe_expiry_alert.get_latest_nightly_version")
+@mock.patch("probe_scraper.probe_expiry_alert.send_emails_for_expiring_probes")
+def test_main_runs_once_per_week(mock_send_emails, mock_get_version, mock_requests_get):
+    mock_requests_get.return_value = ResponseWrapper({})
+    mock_get_version.return_value = "75"
+    for weekday in range(7):
+        base_date = datetime.date(2020, 1, 1)
+        probe_expiry_alert.main(base_date + datetime.timedelta(days=weekday), True)
+
+    mock_send_emails.assert_called_once()
+
+
+@mock.patch("requests.get")
+@mock.patch("probe_scraper.probe_expiry_alert.get_latest_nightly_version")
+@mock.patch("probe_scraper.probe_expiry_alert.send_emails_for_expiring_probes")
+def test_main_run(mock_send_emails, mock_get_version, mock_requests_get):
+    probes = {
+        "p1": {
+            "name": "p1",
+            "history": {
+                "nightly": [{
+                    "expiry_version": "76",
+                    "notification_emails": ["test@email.com"]
+                }]
+            },
+        },
+        "p2": {
+            "name": "p2",
+            "history": {
+                "nightly": [{
+                    "expiry_version": "75",
+                    "notification_emails": ["test@email.com"],
+                }]
+            },
+        }
+    }
+    mock_requests_get.return_value = ResponseWrapper(probes)
+    mock_get_version.return_value = '75'
+
+    probe_expiry_alert.main(datetime.date(2020, 1, 7), True)
+
+    expected_expired_probes = {
+        "p2": [
+            "test@email.com",
+            probe_expiry_alert.DEFAULT_TO_EMAIL,
+        ]
+    }
+    expected_expiring_probes = {
+        "p1": [
+            "test@email.com",
+            probe_expiry_alert.DEFAULT_TO_EMAIL,
+        ]
+    }
+    mock_send_emails.assert_called_once_with(
+        expected_expired_probes, expected_expiring_probes, mock.ANY, True)
