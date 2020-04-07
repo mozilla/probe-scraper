9ae9c77dcb1b6cf7f9bfc709c5300cfacfac79bb
Benjamin Wu <benjaminwu124@gmail.com>
2020-04-07 18:51:15 -0400

    Create bugs for grouped probes

--- probe_scraper/probe_expiry_alert.py
+++ probe_scraper/probe_expiry_alert.py
@@ -8,6 +8,7 @@ from typing import List
 import argparse
 import datetime
 import os
+import re
 import tempfile
 
 import requests
@@ -27,34 +28,28 @@ SCALARS_FILE = "Scalars.yaml"
 EVENTS_FILE = "Events.yaml"
 
 BUG_WHITEBOARD_TAG = "probe-expiry-alert"
-BUG_SUMMARY_TEMPLATE = "Remove or update probe expiring in Firefox {version}: {probe}"
-BUG_RESOLVED_COMMENT_TEMPLATE = "This probe is no longer expiring in Firefox {version}."
+BUG_SUMMARY_TEMPLATE = "Remove or update probes expiring in Firefox {version}: {probe}"
 
 BUG_DESCRIPTION_TEMPLATE = """
-The following Firefox probe will expire in the next major Firefox nightly release: version {version} [1].
+The following Firefox probes will expire in the next major Firefox nightly release: version {version} [1].
 
-`{probe_name}`
+```
+{probes}
+```
 
 What to do about this:
 1. If one, some, or all of the metrics are no longer needed, please remove them from their definitions files (Histograms.json, Scalars.yaml, Events.yaml).
 2. If one, some, or all of the metrics are still required, please submit a Data Collection Review [2] and patch to extend their expiry.
 
-If you have any problems, please ask for help on the #fx-metrics Slack channel. We'll give you a hand.
+If you have any problems, please ask for help on the #fx-metrics Slack channel or the #telemetry Matrix room at https://chat.mozilla.org/#/room/#telemetry:mozilla.org. We'll give you a hand.
 
 Your Friendly, Neighborhood Telemetry Team
 
 [1] https://wiki.mozilla.org/Release_Management/Calendar
 [2] https://wiki.mozilla.org/Firefox/Data_Collection
 
-This is an automated bug created by probe-scraper.  See https://github.com/mozilla/probe-scraper for details.
-
-{notes}
+This is an automated message sent from probe-scraper.  See https://github.com/mozilla/probe-scraper for details.
 """  # noqa
-
-PROBE_LIST_FORMAT_STRING = """
-The following probes {verb} in Firefox version {version}:
-{probes}
-"""
 
 
 @dataclass
@@ -63,20 +58,18 @@ class ProbeDetails:
     product: str
     component: str
     emails: List[str]
+    previous_bug: int
 
     def __eq__(self, other):
         if type(other) == str:
             return self.name == other
         elif type(other) == type(self):
             return self.name == other.name
-        else:
-            raise ValueError(f'Incompatible comparison types: {type(self)} == {type(other)}')
+        raise ValueError(f'Incompatible comparison types: {type(self)} == {type(other)}')
 
 
 def bugzilla_request_header(api_key: str):
-    return {
-        "X-BUGZILLA-API-KEY": api_key
-    }
+    return {"X-BUGZILLA-API-KEY": api_key}
 
 
 def get_bug_component(bug_id: int, api_key: str):
@@ -90,6 +83,7 @@ def get_bug_component(bug_id: int, api_key: str):
 def search_bugs(api_key: str):
     search_query_params = {
         "whiteboard": BUG_WHITEBOARD_TAG,
+        "include_fields": "description,summary",
     }
     response = requests.get(BUGZILLA_BUG_URL, params=search_query_params,
                             headers=bugzilla_request_header(api_key))
@@ -97,33 +91,60 @@ def search_bugs(api_key: str):
     return response.json()["bugs"]
 
 
+def get_longest_prefix(values, tolerance=0):
+    """
+    Return the longest matching prefix among the list of strings.
+    If a prefix is less than 2 characters, return the first string.
+    Tolerance allows some characters to not match.
+    """
+    if len(values) == 1:
+        return values[0]
+    if len(values) == 0:
+        return ''
+
+    longest_match = []
+    for c in zip(*values):
+        if len(set(c)) > min([1 + tolerance, len(values) - 1]):
+            break
+        longest_match.append(c[0])
+
+    if len(longest_match) < 2:
+        return values[0]
+
+    return ''.join(longest_match) + '*'
+
+
 def create_bug(probes: List[ProbeDetails], version: str,  api_key: str):
-    description_notes = ("No emails associated with the probe have a "
-                         "corresponding Bugzilla account." if len(probes[0].emails) == 0 else "")
-    # TODO: THIS WHOLE THING
-    #create_params = {
-    #    "product": "Toolkit",
-    #    "component": "Telemetry",
-    #    "summary": BUG_SUMMARY_TEMPLATE.format(version=version, probe=probe_name),
-    #    "description": BUG_DESCRIPTION_TEMPLATE.format(
-    #        version=version, probe_name=probe_name, notes=description_notes),
-    #    "version": "unspecified",
-    #    "type": "task",
-    #    "whiteboard": BUG_WHITEBOARD_TAG,
-    #    "flags": [
-    #        {
-    #            "name": "needinfo",
-    #            "type_id": 800,
-    #            "status": "?",
-    #            "requestee": email,
-    #        }
-    #        for email in emails
-    #    ],
-    #}
-    #create_response = requests.post(BUGZILLA_BUG_URL, json=create_params,
-    #                                headers=headers=bugzilla_request_header(api_key))
-    #create_response.raise_for_status()
-    #print(f"Created bug {str(create_response.json())} for probe {probe_name}")
+    probe_names = [probe.name for probe in probes]
+    probe_prefix = get_longest_prefix(probe_names, tolerance=1)
+
+    create_params = {
+        "product": probes[0].product,
+        "component": probes[0].component,
+        "summary": BUG_SUMMARY_TEMPLATE.format(version=version, probe=probe_prefix),
+        "description": BUG_DESCRIPTION_TEMPLATE.format(
+            version=version, probes="\n".join(probe_names)),
+        "version": "unspecified",
+        "type": "task",
+        "whiteboard": BUG_WHITEBOARD_TAG,
+        "see_also": 1396144,  # TODO: probes[0].previous_bug,
+        "flags": [
+            {
+                "name": "needinfo",
+                "type_id": 800,
+                "status": "?",
+                "requestee": email,
+            }
+            for email in probes[0].emails
+        ],
+    }
+    create_response = requests.post(BUGZILLA_BUG_URL, json=create_params,
+                                    headers=bugzilla_request_header(api_key))
+    try:  # TODO: remove catch
+        create_response.raise_for_status()
+    except requests.HTTPError:
+        print(create_response.json())
+    print(f"Created bug {str(create_response.json())} for {probe_prefix}")
 
 
 def check_bugzilla_user_exists(email: str, api_key: str):
@@ -162,12 +183,16 @@ def find_expiring_probes(probes: dict, target_version: str,
         expiry_version = details["expiry_version"]
 
         if expiry_version == target_version:
-            product, component = (
-                get_bug_component(max(details["bug_numbers"]), bugzilla_api_key)
-                if len(details["bug_numbers"]) > 0 else ("Firefox", "General")
-            )
+            if len(details["bug_numbers"]) == 0:
+                last_bug_number = None
+                product = "Firefox"
+                component = "General"
+            else:
+                last_bug_number = max(details["bug_numbers"])
+                product, component = (get_bug_component(last_bug_number, bugzilla_api_key))
             expiring_probes.append(
-                ProbeDetails(name, product, component, details.get("notification_emails", [])))
+                ProbeDetails(name, product, component,
+                             details.get("notification_emails", []), last_bug_number))
 
     return expiring_probes
 
@@ -182,19 +207,27 @@ def file_bugs(probes: List[ProbeDetails], version: str,
         - if no bug exists for a given probe, create a bug
     """
     found_bugs = search_bugs(bugzilla_api_key)
-    print(f"Found {len(found_bugs)} previously created bugs for version {version}")
 
     new_expiring_probes = probes.copy()
-    #for bug in found_bugs:
-    #    probe_name = bug["summary"].split()[-1]
-    #    if probe_name in [probe.name for probe in probes]:
-    #        new_expiring_probes.remove(probe_name)
+    for bug in found_bugs:
+        # Regex for version and probe names requires bug description to be BUG_DESCRIPTION_TEMPLATE
+        if re.search("release: version (\d+)", bug["description"]).group(1) != version:
+            continue
+        probes_in_bug = re.search("```(.*)```", bug["description"], re.DOTALL).group(1).split()
+        for probe_name in probes_in_bug:
+            if probe_name in [probe.name for probe in probes]:
+                new_expiring_probes.remove(probe_name)
+
+    print(f"Found previously created bugs for {len(probes) - len(new_expiring_probes)} probes "
+          f"for version {version}")
 
     # group by component and email
     probes_by_component_by_email_set = defaultdict(list)
     for probe in new_expiring_probes:
         probes_by_component_by_email_set[
             (probe.product, probe.component, ','.join(sorted(probe.emails)))].append(probe)
+
+    print(f"creating {len(probes_by_component_by_email_set)} new bugs")
 
     for grouping, probe_group in probes_by_component_by_email_set.items():
         if create_bugs:
@@ -229,14 +262,14 @@ def main(current_date: datetime.datetime, dryrun: bool, bugzilla_api_key: str):
     # Only send create bugs on Wednesdays, run the rest for debugging/error detection
     create_bugs = not dryrun and current_date.weekday() == 2
 
-    # find emails with no bugzilla account and group probes by bug component and emails
+    # find emails with no bugzilla account
     emails_wo_accounts = defaultdict(list)
-
     for probe_name, email_list in [(probe.name, probe.emails) for probe in expiring_probes]:
         for i, email in enumerate(email_list.copy()):
-            if not check_bugzilla_user_exists(email, bugzilla_api_key):
+            if (email in emails_wo_accounts.keys() or
+                    not check_bugzilla_user_exists(email, bugzilla_api_key)):
                 emails_wo_accounts[email].append(probe_name)
-                email_list.pop(i)
+                email_list.remove(email)
 
     file_bugs(expiring_probes, next_version, bugzilla_api_key, create_bugs=create_bugs)
 
