bb6502b7d59c8361ad4fb80db5c4611066bb8bed
LilyLME <brilyjoy@gmail.com>
2020-10-15 20:55:31 +0100

    improved error output
    Signed-off-by: LilyLME <brilyjoy@gmail.com>

--- probe_scraper/check_repositories.py
+++ probe_scraper/check_repositories.py
@@ -11,7 +11,7 @@ REPOSITORIES = os.path.join(
 )
 validation_errors = []
 with open(REPOSITORIES) as data:
-    repos = yaml.load(data, Loader=yaml.FullLoader)
+    repos = yaml.load(data, Loader=yaml.SafeLoader)
     for repo in repos:
         repo_url = repos[repo]["url"]
         branch = "master"
@@ -29,10 +29,8 @@ with open(REPOSITORIES) as data:
                 + "/"
                 + metrics_files[0]
             )
-            print(f"Downloading {repo} metrics_file from {temp_url}...")
             response = reqs.get(temp_url)
             assert response.status_code == 200
-            print(f"Checking {repo} metrics_file {metrics_files[0]}...\n")
             with open("temp-metrics.yaml", "w") as filehandle:
                 filehandle.write(response.text)
             yaml_lint_errors = open("yaml-lint-errors.txt", "w")
@@ -40,13 +38,17 @@ with open(REPOSITORIES) as data:
                 [Path("./temp-metrics.yaml")], yaml_lint_errors, {}
             )
             if temp_erros:
-                print(f"Errors found in {repo} metrics file : {print(temp_erros)}")
                 if "prototype" in repos[repo] and not repos[repo]["prototype"]:
                     validation_errors.append({"repo": repo, "errors": temp_erros})
     os.remove("temp-metrics.yaml")
     os.remove("yaml-lint-errors.txt")
     if validation_errors:
-        print(
-            f"\nSummary of validation errors \n==========================\n {validation_errors}"
-        )
+        print("\nSummary of validation errors \n====================================\n")
+        print(f"{len(validation_errors)} repositories had problems\n")
+        for error in validation_errors:
+            print(
+                f"\nErrors found in {error['repo']} \n====================================\n"
+            )
+            for line_errors in error["errors"]:
+                print(line_errors)
         exit(1)
