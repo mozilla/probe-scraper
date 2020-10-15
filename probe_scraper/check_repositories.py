b54f0a867039b1c13a10595905491d797e303ba3
LilyLME <brilyjoy@gmail.com>
2020-10-15 18:24:14 +0100

    added metric file validation with glean_parser
    Signed-off-by: LilyLME <brilyjoy@gmail.com>

--- probe_scraper/check_repositories.py
+++ probe_scraper/check_repositories.py
@@ -1,13 +1,15 @@
 import os
+from pathlib import Path
 
 import requests as reqs
 import yaml
+from glean_parser.lint import lint_yaml_files
 
 GITHUB_RAW_URL = "https://raw.githubusercontent.com"
 REPOSITORIES = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "repositories.yaml"
 )
-
+validation_errors = []
 with open(REPOSITORIES) as data:
     repos = yaml.load(data, Loader=yaml.FullLoader)
     for repo in repos:
@@ -27,6 +29,20 @@ with open(REPOSITORIES) as data:
                 + "/"
                 + metrics_files[0]
             )
-            print("Checking {} metrics_file at {} ...".format(repo, temp_url))
-        response = reqs.get(temp_url)
-        assert response.status_code == 200
+            print(f"Downloading {repo} metrics_file from {temp_url}...")
+            response = reqs.get(temp_url)
+            assert response.status_code == 200
+            print(f"Checking {repo} metrics_file {metrics_files[0]}...\n")
+            with open("temp-metrics.yaml", "w") as filehandle:
+                filehandle.write(response.text)
+            yaml_lint_errors = open("yaml-lint-errors.txt", "w")
+            temp_erros = lint_yaml_files(
+                [Path("./temp-metrics.yaml")], yaml_lint_errors, {}
+            )
+            if temp_erros:
+                print(f"Errors found in {repo} metrics file : {print(temp_erros)}")
+                validation_errors.append(temp_erros)
+    os.remove("temp-metrics.yaml")
+    os.remove("yaml-lint-errors.txt")
+    if validation_errors:
+        exit(1)
