ea0773c299f4f1d311ad9769ba2abb1a92b3908c
Marlene-M-Hirose <mhirose@mozilla.com>
2021-11-08 16:47:21 -0800

    fixes #348 Add test that repositories.yaml only contains any references to libraries that exist

---
+++ tests/test_library_refs.py
@@ -0,0 +1,10 @@
+import yaml
+
+def test_library_refs():
+  yaml_file = open("repositories.yaml", "r")
+  repositories = yaml.safe_load(yaml_file)
+  yaml_file.close()
+  libs = set(library["library_name"] for library in repositories["libraries"])
+  for app in repositories["applications"]:
+    if missing_libs := set(app["dependencies"]) - libs:
+      raise KeyError(f'application {app["app_name"]} contains invalid library references: {missing_libs}')
