2ed31f18ddb2c949a3d45b8daa0145565f6af727
Marlene Hirose <92952117+Marlene-M-Hirose@users.noreply.github.com>
2021-11-10 11:36:15 -0800

    Update tests/test_library_refs.py
    Co-authored-by: Daniel Thorn <dthorn@mozilla.com>

--- tests/test_library_refs.py
+++ tests/test_library_refs.py
@@ -7,7 +7,8 @@ def test_library_refs():
     yaml_file.close()
     libs = set(library["library_name"] for library in repositories["libraries"])
     for app in repositories["applications"]:
-        if missing_libs := set(app["dependencies"]) - libs:
+        missing_libs = set(app["dependencies"]) - libs
+        if missing_libs:
             raise KeyError(
                 f'application {app["app_name"]} contains invalid library references: {missing_libs}'
             )
