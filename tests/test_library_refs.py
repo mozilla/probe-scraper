7749cae4b75741a7ecca207b5b26074db7aca31b
Marlene-M-Hirose <mhirose@mozilla.com>
2021-11-10 15:38:19 -0800

    update code to check the proper dependency names

--- tests/test_library_refs.py
+++ tests/test_library_refs.py
@@ -5,7 +5,11 @@ def test_library_refs():
     yaml_file = open("repositories.yaml", "r")
     repositories = yaml.safe_load(yaml_file)
     yaml_file.close()
-    libs = set(library["library_name"] for library in repositories["libraries"])
+    dependency_list = []
+    for library in repositories["libraries"]:
+        for variant in library["variants"]:
+            dependency_list.append(variant["dependency_name"])
+    libs = set(dependency_list)
     for app in repositories["applications"]:
         missing_libs = set(app["dependencies"]) - libs
         if missing_libs:
