08cca48c6543979b869cbe18a3099a2bb770cab7
Daniel Thorn <dthorn@mozilla.com>
2021-11-15 15:23:07 -0800

    Address review from #368

--- tests/test_library_refs.py
+++ tests/test_library_refs.py
@@ -5,11 +5,10 @@ def test_library_refs():
     yaml_file = open("repositories.yaml", "r")
     repositories = yaml.safe_load(yaml_file)
     yaml_file.close()
-    dependency_list = []
+    libs = set()
     for library in repositories["libraries"]:
         for variant in library["variants"]:
-            dependency_list.append(variant["dependency_name"])
-    libs = set(dependency_list)
+            libs.add(variant["dependency_name"])
     for app in repositories["applications"]:
         missing_libs = set(app["dependencies"]) - libs
         if missing_libs:
