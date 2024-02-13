4559963a2f80d2f18490f3e35cae4f3d32b2e496
Chris H-C <chutten@mozilla.com>
2024-02-13 16:32:10 -0500

    bug 1877836 - Remove GeckoView Streaming Telemetry
    And the GVST-specific easing of the duplicate check.

--- probe_scraper/glean_checks.py
+++ probe_scraper/glean_checks.py
@@ -137,13 +137,9 @@ def check_for_duplicate_metrics(repositories, metrics_by_repo, emails):
                 if metric["history"][-1]["dates"]["last"] == last_timestamp:
                     metric_sources.setdefault(metric_name, []).append(dependency)
 
-        duplicate_sources = {}
-        for (k, v) in metric_sources.items():
-            # Exempt cases when one of the sources is Geckoview Streaming to
-            # avoid false positive duplication accross app channels.
-            v = [dep for dep in v if "engine-gecko" not in dep]
-            if len(v) > 1:
-                duplicate_sources[k] = v
+        duplicate_sources = dict(
+            (k, v) for (k, v) in metric_sources.items() if len(v) > 1
+        )
 
         if not len(duplicate_sources):
             continue
