463d4a7f798abcaa5c7a8d769d7a55e175d4a220
Jan-Erik Rediger <janerik@fnordig.de>
2026-07-21 16:06:41 -0400

    fog-updater: Always enable auto-merge on the newly created pull request

--- fog-updater/src/fog_updater/__init__.py
+++ fog-updater/src/fog_updater/__init__.py
@@ -267,6 +267,10 @@ def run(argv, repo, author, debug=False, dry_run=False):
         head=pr_branch_name,
         base=release_branch_name,
     )
+
+    # Enable auto-merge on the new pull request
+    pr.enable_automerge(merge_method="REBASE")
+
     print(f"{ts()} Pull request at {pr.html_url}")
 
 
